3e5ac2b11a0af70a5d166cc5ff1bd0e3987a3e20
maybemnv <manavkauahal99@gmail.com>
2026-01-01 21:47:38 +0530

    RM: Knowledge Base Docs and ingestion structure Due to Model changes

--- src/embeddings/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Embeddings Module - Text Embedding Generation
-
-This module provides text embedding functionality using SentenceTransformers.
-"""
-
-from src.embeddings.encoder import EmbeddingEncoder
-
-__all__ = ["EmbeddingEncoder"]
